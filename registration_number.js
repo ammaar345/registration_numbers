71c9ec92f13ac86a92b79404e5db1e5e5b630fe5
Ammaar <fakirammaar8@gmail.com>
2020-06-02 13:33:29 +0200

    added a grid

--- registration_number.js
+++ registration_number.js
@@ -1,2 +1,33 @@
-const button=document.querySelector(".btnAdd");
-const text=document.querySelector(".regNumber");
+const button = document.querySelector(".btnAdd");
+const text = document.querySelector(".regNumber");
+//const townVal=document.querySelector(".town-select");
+const town = document.getElementById("townSelect");
+function append() {
+var textVal=text.value;
+   if (textVal) {
+      var townVal = town.options[town.selectedIndex].value;
+      console.log(townVal);
+      var node = document.createElement("li");
+      var textnode = document.createTextNode(text.value);
+      node.appendChild(textnode);
+
+      if (textVal.startsWith("CAA")) {
+         document.getElementById("List").appendChild(node)
+      }
+
+      else if (textVal.startsWith("CJ")) {
+         document.getElementById("List2").appendChild(node)
+      }
+
+   }
+   else if (textVal.startsWith("CAG")) {
+      document.getElementById("List3").appendChild(node)
+   }
+   else if (textVal.startsWith("CL")){
+      document.getElementById("List4").appendChild(node)
+   }
+   //console.log(textnode)
+}
+
+
+button.addEventListener("click", append)
