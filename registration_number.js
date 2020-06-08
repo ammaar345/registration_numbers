78c433314588e990fae77b79998a6ea1aa7d4a68
Ammaar <fakirammaar8@gmail.com>
2020-06-08 11:44:17 +0200

    added new functions

--- registration_number.js
+++ registration_number.js
@@ -1,54 +1,62 @@
-let regState;
-if (localStorage["Plates"]) {
-   regState = JSON.parse(localStorage['Plates']);
+//
+var storedPlates = localStorage["Plates"]
+let regNumbers = [];
+var arrAll=[];
+if (storedPlates) {
+   regNumbers = JSON.parse(storedPlates)
+
+
 }
-const plateFunc = RegNumber(regState);
+const plateFunc = RegNumber(regNumbers);
 //
 const button = document.querySelector(".btnAdd");
 const text = document.querySelector(".regNumber");
-//const townVal=document.querySelector(".town-select");
-const item1 = document.getElementById("List1");
 const town = document.getElementById("townSelect");
-const storedRegState = localStorage["Plates"];
+//const storedRegState = localStorage["Plates"];
 
-for (plateNo in plateFunc.plateStorage()) {
-   item1.innerHTML = plateFunc.objToString();
-} //how to display in a list
+
+
+
+const regNumList = document.querySelector(".regNumList");
+regNumList.innerHTML = "";
+var node = document.createElement("li");
+// loop over a list of reg numbers
+
+for (var i = 0; i < regNumbers.length; i++) {//array from main to go in for loop
+   const currentReg = regNumbers[i];
+   node.innerHTML = currentReg; 
+   regNumList.appendChild(node);
+ // 
+
+}
+
+
 function append() {
    var textVal = text.value;
    var townVal = town.options[town.selectedIndex].value;
-   var node = document.createElement("li");
-   var textnode = document.createTextNode(textVal);
+  
+  // var textnode = document.createTextNode(textVal);
 
-   plateFunc.numRegister(textVal);
-   if (textVal) {
+   plateFunc.addRegNumber(textVal);
+   plateFunc.filter(townVal,arrAll)
+// console.log(town.value)
+// if (town.value){
+// if (townVal="all"){
 
-      //item1.parentNode.removeChild(item1)
-      if (textVal.startsWith("CA") && (textVal.length === 10)) {
+// }
 
-         node.appendChild(textnode);
-         item1.appendChild(node);
-      }
-      else if (textVal.startsWith("CJ") && (textVal.length === 10)) {
-         node.appendChild(textnode);
-         item1.appendChild(node);
 
-      }
-      else if (textVal.startsWith("CL") && (textVal.length === 10)) {
-         node.appendChild(textnode);
-         item1.appendChild(node);
-      }
-      // else if (textVal.startsWith("CAG"){(george)
-      //   item1.appendChild(node);
-      // }
-      // console.log(townVal);
-      // console.log(textnode);
-      // console.log(textVal.length);
+// }
+
+  
+      ///
+     
+    
       var storingPlates = plateFunc.plateStorage();
       var regPlates = JSON.stringify(storingPlates);
       localStorage['Plates'] = regPlates;
 
-   }
+
    // if(townVal=1){
 
    // }
--- registration_numbers-factory.js
+++ registration_numbers-factory.js
@@ -1,63 +1,77 @@
 function RegNumber(initialState) {
-    var regNumbers = initialState ? initialState : {};
-    function numRegister(numPlate) {
-        //when the greet button is pressed check if this user was already greeted before
-        //by looking if the userName exists in regNumbers if not increment this counter and update the screen
 
-        if (regNumbers[numPlate] === undefined) {
+    var regNumbers = initialState ? initialState : [];
+    // function objToString() {
+    //     for (var i = 0; i < plateFunc.numberPlates(); i++) {
+    //         var pl = Object.keys(regNumbers)[i];
+    //         //var listPlate=regNumbers[i];
 
-            //add an entry for the user that was greeted in the Object Map
-            regNumbers[numPlate] = 1;
 
-            //regNumbers["Plate"]=numPlate;
-        }
-    }
+    //     }
+    //     // console.log(pl)
+    //     return pl
+    // }
+
+    // function numberPlates() {
+    //     var platesCount = Object.keys(regNumbers).length
+
+    //     return platesCount
+    //     //use to loop through obj
+    // }
+    // function numRegister(numPlate) {
+    //     //when the greet button is pressed check if this user was already greeted before
+    //     //by looking if the userName exists in regNumbers if not increment this counter and update the screen
+
+    //     if (regNumbers[numPlate] === undefined) {
+
+    //         //add an entry for the user that was greeted in the Object Map
+    //         regNumbers[numPlate] = 1;
+
+    //         //regNumbers["Plate"]=numPlate;
+    //     }
+    // }
     function plateStorage() {
-
-        //var plates= JSON.stringify(regNumbers);
-        //console.log(plateFunc.numberPlates())      
         console.log(regNumbers)
-        //how to add a title while adding the plate number
         return regNumbers;
 
     }
-    function objToString() {
-        for (var i = 0; i < plateFunc.numberPlates(); i++) {
-            var pl = Object.keys(regNumbers)[i];
-            //var listPlate=regNumbers[i];
 
+    function addRegNumber(regNumber) {
+        //still need to filter out if its invalid **
 
+        if (regNumber.startsWith("CJ") || ("CL") || ("CY") && regNumber.length === 10) {
+            if (!regNumbers.includes(regNumber) && regNumber !== "") {
+                regNumbers.push(regNumber);
+                return true;
+            }
+            return false;
         }
-        // console.log(pl)
-        return pl
-    }
-    // function allPaarl(){
-    //for (var i=0;i<plateFunc.numberPlates();i++){
-
-    //}
-    // }
-    // function allCape(){
-
-    // for (var i=0;i<plateFunc.numberPlates();i++){
-
-    // } 
-    // }
-    // function allStellies(){
-    // for (var i=0;i<plateFunc.numberPlates();i++){
-
-    // }
-    // }
-    function numberPlates() {
-        var platesCount = Object.keys(regNumbers).length
-
-        return platesCount
-        //use to loop through obj
     }
 
+    function filter(location, arrAll) {
+        // CL, CY or contains(obtained from dropdown menu)
+        var filteredList = [];
+        for (var i = 0; i < regNumbers.length; i++) {
+            var currentReg = regNumbers[i]; //if no work make var = const
+            if (currentReg.startsWith(location)) {
+                filteredList.push(currentReg);
+            }
+            else if (currentReg.startsWith("all")) {
+                for (var i = 0; i < regNumbers.length; i++) {
+                  var regOfAll=regNumbers[i];
+                    arrAll.push(regOfAll);
+                }
+           return arrAll }
+        }
+        return filteredList;
+    }
     return {
-        numRegister,
-        numberPlates,
+        addRegNumber,
+        //numRegister,
+        //  numberPlates,
         plateStorage,
-        objToString
+        filter
+        //  objToString,
+
     }
 }
