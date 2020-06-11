16ca27b4d8b152f0340a07933a063702caaa345e
Ammaar <fakirammaar8@gmail.com>
2020-06-11 12:48:12 +0200

    added functions

--- registration_numbers-factory.js
+++ registration_numbers-factory.js
@@ -1,12 +1,13 @@
 function RegNumber(initialState) {
 
     var regNumbers = initialState ? initialState : [];
-
+    
     function plateStorage() {
         console.log(regNumbers)
         return regNumbers;
 
     }
+
     function testfilter(location, registration) {
         // CL, CY or contains
         var filteredList = [];
@@ -31,58 +32,132 @@ function RegNumber(initialState) {
             return true
         }
     }
-        function addRegNumber(regNumber) {
-            //still need to filter out if its invalid **  
-            if (regNumber !== "" && /C[YLJ] \d{3,5}/.test(regNumber) || /C[YLJ] \d+-\d+/.test(regNumber)) {
-                if (!regNumbers.includes(regNumber)) {
+    function addRegNumber(regNumber) {
+        //still need to filter out if its invalid **  
+        if (regNumber !== "" && /C[YLJ] \d{3,5}/.test(regNumber) || /C[YLJ] \d+-\d+/.test(regNumber)) {
+            if (!regNumbers.includes(regNumber)) {
 
-                    regNumbers.push(regNumber);
-                    return true;
-                }
+                regNumbers.push(regNumber);
+                return true;
             }
-            return false;
-
         }
+        return false;
 
-        // if (!regNumbers.includes(regNumber) && regNumber !== "") {
-        //     if (regNumber.startsWith("CJ") && regNumber.length === 10 || regNumber.startsWith("CL") && regNumber.length === 10 || regNumber.startsWith("CY") && regNumber.length === 10) {
-
-
-        //         regNumbers.push(regNumber);
-        //     }
-        // }
-
-        function filter(location) {
-            // CL, CY or contains(obtained from dropdown menu)
-            var filteredList = [];
-            for (var i = 0; i < regNumbers.length; i++) {
-                var currentReg = regNumbers[i]; //if no work make var = const
-                if (town.value) {
-                    if (currentReg.startsWith(location)) {
-                        filteredList.push(currentReg);
-                    } else if (location === "all") {
-
-                        filteredList.push(currentReg);
-                    }
-
-
-                }
-
-
-            }
-
-            return filteredList
-
-        }
-        return {
-            addRegNumber,
-            //numRegister,
-            //  numberPlates,
-            plateStorage,
-            filter,
-            checkExists,
-            testfilter,
-            //  objToString,
-            checkValid
-        }
     }
+
+    // if (!regNumbers.includes(regNumber) && regNumber !== "") {
+    //     if (regNumber.startsWith("CJ") && regNumber.length === 10 || regNumber.startsWith("CL") && regNumber.length === 10 || regNumber.startsWith("CY") && regNumber.length === 10) {
+
+
+    //         regNumbers.push(regNumber);
+    //     }
+    // }
+
+    function filter(location) {
+        // CL, CY or contains(obtained from dropdown menu)
+        var filteredList = [];
+        for (var i = 0; i < regNumbers.length; i++) {
+            var currentReg = regNumbers[i]; //if no work make var = const
+            if (town.value) {
+                if (currentReg.startsWith(location)) {
+                    filteredList.push(currentReg);
+                } else if (location === "all") {
+
+                    filteredList.push(currentReg);
+                }
+
+
+            }
+
+
+        }
+
+        return filteredList
+
+    }
+    function checkText(registerNum) {
+
+        if (registerNum === "") {
+            // msg.classList.add("failed") 
+            return "Please enter a registration number."
+        }
+        else if (!checkValid(registerNum)) {
+            //msg.classList.add("failed") 
+            return "The format of this registration number is incorrect."
+        }
+
+        else if (!checkExists(registerNum, plateStorage())) {
+            // msg.classList.add("failed") 
+            return "This registration number is already taken."
+        }
+
+        else if(checkExists(registerNum,plateStorage())) {
+           // msg.classList.add("success")
+            return "Entry has been successfully added."
+        }
+
+    }
+    function classAdd(registerN) {
+        var result=""
+        if (checkExists(registerN,plateStorage())){
+            result="success"
+                        }
+         else if (checkText() == "Please enter a registration number.") {
+             result="failed"
+           
+        }
+        
+         else if (checkText() =="The format of this registration number is incorrect.") {
+            result="failed"
+            
+        }
+        else  if (checkText()== "This registration number is already taken.") {
+            result="failed"
+        
+        }
+ 
+       return result
+       
+        
+    }
+    function classAddTest() {
+        var returnResult=""
+        if (checkText()==="Entry has been successfully added."){
+            returnResult="success"
+                        }
+         else if (checkText() === "Please enter a registration number.") {
+            returnResult="failed"
+           
+        }
+        
+         else if (checkText() ==="The format of this registration number is incorrect.") {
+            returnResult="failed"
+            
+        }
+        else  if (checkText()=== "This registration number is already taken.") {
+            returnResult="failed"
+        
+        }
+ 
+       return returnResult
+       
+        
+    }
+
+
+
+    return {
+        addRegNumber,
+        //numRegister,
+        //  numberPlates,
+        plateStorage,
+        filter,
+        checkExists,
+        testfilter,
+        //  objToString,
+        checkValid,
+        checkText,
+        classAdd,
+        classAddTest
+    }
+}
