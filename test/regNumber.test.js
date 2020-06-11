970bf88f31a17c24b071beeb5b115233b935b3a1
Ammaar <fakirammaar8@gmail.com>
2020-06-11 14:46:12 +0200

    added better descriptions for tests

--- test/regNumber.test.js
+++ test/regNumber.test.js
@@ -52,16 +52,16 @@ describe("Testing filter function based on location.", function () {
 	});
 
 });
-describe("Testing the message being returned based on the registration number entered.", function () {
+describe("Testing if the correct message is being returned based on the registration number being entered.", function () {
 
-	it('should return "This registration number is already taken." .', function () {
+	it('should return "This registration number is already taken." if the registration number exists already.', function () {
 		const arrayMain = ["CJ 8913", "CJ 781", "CL 2152", "CL 913", "CL 932-122", "CJ 1232", "CY 435", "CJ 90012"];
 		const regPlate = RegNumber(arrayMain);
 		var plate = "CJ 90012";
 		var msg = regPlate.checkText(plate)
 		assert.equal("This registration number is already taken.", msg);
 	});
-	it('should return "Entry has been successfully added." .', function () {
+	it('should return "Entry has been successfully added." if the registration number is not taken and follows the correct format.  .', function () {
 		const arrayMain = ["CJ 8913", "CJ 781", "CL 2152", "CL 913", "CL 932-122", "CJ 1232", "CY 435", "CJ 90012"];
 		const regPlate = RegNumber(arrayMain);
 		var plate = "CJ 5512";
@@ -69,7 +69,7 @@ describe("Testing the message being returned based on the registration number en
 
 		assert.deepEqual("Entry has been successfully added.", msg);
 	});
-	it('should return "The format of this registration number is incorrect." .', function () {
+	it('should return "The format of this registration number is incorrect." if the format of the registration number is incorrect.', function () {
 		const arrayMain = ["CJ 8913", "CJ 781", "CL 2152", "CL 913", "CL 932-122", "CJ 1232", "CY 435", "CJ 90012"];
 		const regPlate = RegNumber(arrayMain);
 		var plate = "CJ25512";
@@ -78,7 +78,7 @@ describe("Testing the message being returned based on the registration number en
 
 		assert.deepEqual("The format of this registration number is incorrect.", msg);
 	});
-	it('should return "Please enter a registration number." .', function () {
+	it('should return "Please enter a registration number." if the text box is left empty.', function () {
 		const arrayMain = ["CJ 8913", "CJ 781", "CL 2152", "CL 913", "CL 932-122", "CJ 1232", "CY 435", "CJ 90012"];
 		const regPlate = RegNumber(arrayMain);
 		var plate = "";
